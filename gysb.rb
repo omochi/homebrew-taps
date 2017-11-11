b1f490215190a717963cacf1aaf6549f9af00542
omochi <omochi.metaru@gmail.com>
2017-11-11 21:58:33 +0900

    tap

--- gysb.rb
+++ gysb.rb
@@ -1,8 +1,8 @@
 class Gysb < Formula
   desc "Generate your swifty boilerplate"
   homepage ""
-  url "https://github.com/omochi/gysb/archive/0.8.8.tar.gz"
-  sha256 "f9198809d87e0cb9d9c528f3fb6786dcecd772e5a871947bf85edfb62ba7c818"
+  url "https://github.com/omochi/gysb/archive/0.8.9.tar.gz"
+  sha256 "e3fe50e25dcd365160fe7f1c90cbb31ae115114fa9f86dc249d006e7b51f7524"
 
   def install
     system "make", "install", "PREFIX=#{prefix}"
